e8de9176cd376a4a3176b1d84400846e70a2dcf0
BHou <daily.bhou@gmail.com>
2013-05-26 19:02:50 +0200

    add merge function to server

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,9 @@
  */
 var schedule = require('node-schedule');
 var http = require('http');
+var fs = require('fs');
 
+var SCRAPY_ROOT = '/Users/bohou/Developments/git_repository/Bo-AntibesBusCrawler/AntibesBusCrawler';
 
 // http server to provide the data access
 function serverCallback(req, res){
@@ -76,11 +78,76 @@ function startBusMapCrawlEx(args){
 	child.unref();
 }
 
-function mergeBusMap(args){
-	console.log('start merge job');
+// merge the src list to the dest list
+function merge(src, dest) {
+	var len = src.length;
+
+	var existed = false;
+	var directionExisted = false;
+
+	var destDirections = null;
+	var srcDirections = null;
+
+	for (var i = 0; i < len; i++) {
+		existed = false;
+		// check if dest contains the stop
+		for (var j = 0; j < dest.length; j++) {
+			if (src[i].code == dest[j].code) {
+				existed = true;
+
+				// merge the directions
+				srcDirects = src[i].directions == null ? [] : src[i].directions;
+				destDirects = dest[j].directions == null ? [] : dest[j].directions;
+				for (var u = 0; u < srcDirects.length; u++) {
+					directionExisted = false;
+					for (var v = 0; v < destDirects.length; v++) {
+						if (srcDirects[u].code == destDirects[v].code && 
+							srcDirects[u].lineNo == destDirects[v].lineNo &&
+							srcDirects[u].name == destDirects[v].name) {
+							directionExisted = true;
+							break;
+						}
+					}
+					if (!directionExisted) {
+						destDirects.push(srcDirects[u]);
+					}
+				}
+
+				break;
+			}
+		}
+		if (!existed) {
+			dest.push(src[i]);
+		}
+	}
 }
 
+function mergeBusMap(args){
+	console.log('start merge job');
+	var list = args['list'];
+	var len = list.length;
 
+	// load all the files
+	var files = [];
+	for (var i = 0; i < len; i++) {
+		files.push(require(SCRAPY_ROOT+'/'+list[i]));
+	}
+
+	// merge them
+	var ret = files[0];
+	for (var i = 1; i < len; i++) {
+		merge(files[i], ret);
+	}
+
+	// write to file
+	fs.writeFile(SCRAPY_ROOT + '/mergedStop.json', JSON.stringify(ret, null, 4), function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("save merge result to: " + SCRAPY_ROOT + '/mergedStop.json');
+		}
+	});
+}
 
 
 /**
@@ -92,7 +159,7 @@ server.listen(8000);
 console.log('Server running at http://127.0.0.1:8000');
 
 
-var minute = 58;
+var minute = 01;
 // job to access the Valbonne bus map, scheduled every day morning at 2:00 am
 scheduleJob(14, minute, startBusMapCrawlEx, 
 	{'url':'http://tempsreel.envibus.fr/list/?com_id=3&letter=*', 'output':'valbonne.json'});
@@ -102,4 +169,4 @@ scheduleJob(15, minute, startBusMapCrawlEx,
 	{'url':'http://tempsreel.envibus.fr/list/?com_id=1&letter=*', 'output':'antibes.json'});
 
 // job to merge all the bus map to one map, scheduled every day morning at 5:00 am
-scheduleJob(16, minute, mergeBusMap, {});
+scheduleJob(19, minute, mergeBusMap, {'list':['antibes.json', 'valbonne.json', 'biot.json']});
